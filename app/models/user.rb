04588baa27e7eff91b4651b99c4f0eb5180fc6a5
bibek244 <bibekgosai555@gmail.com>
2024-08-20 10:50:14 +0545

    added devise

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         has_many :memberships 
+         has_many :organizations, through: :memberships
+         has_many :posts, dependent: :destroy
+         has_one :profile, dependent: :destroy
+        #  has_many :comments, dependent: :destory
+end
