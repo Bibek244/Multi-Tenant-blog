3c3d5190c1303b9df6c9d7013b5e99dbc8943d63
bibek244 <bibekgosai555@gmail.com>
2024-08-20 12:39:27 +0545

    handle user registration of user who already exist in other organization

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,37 @@
+class RegistrationsController < Devise::RegistrationsController
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user.present?
+      organization = Organization.find_by(id: params[:user][:organization_id])
+
+      if user.valid_password?(params[:user][:password])
+        Membership.find_or_create_by(user: user, organization: organization)
+        sign_in_and_redirect user,  notice: "successfully added #{user.email} to #{organization.name}"
+      else
+        redirect_to new_user_registration_url(organization_id: organization.id, organization_name: organization.name, email: user.email), alert: 'incorrect password for the existing user'
+      end
+
+    else
+      @user = User.new(signup_params)
+      @organization = Organization.find_by(id: params[:user][:organization_id])
+
+      if @organization.present?
+        if @user.save
+          Membership.find_or_create_by(user: @user, organization: @organization)
+          flash[:notice] = "You've been added to the organization."
+          sign_in_and_redirect @user, event: :authentication
+        else
+          render :new
+        end
+      else
+        super
+      end
+    end
+  end
+
+  private
+
+  def signup_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
