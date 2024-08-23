5336246cdc5d09f1fcca2ad885a5302b5773d8b0
bibek244 <bibekgosai555@gmail.com>
2024-08-23 13:33:47 +0545

    refactor session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 # app/controllers/users/sessions_controller.rb
 class SessionsController < Devise::SessionsController
   def create
-    user = User.find_by(email: params[:user][:email])
+    service = Users::UserSessionService.new(
+      email: params[:user][:email],
+      password: params[:user][:password],
+      organization_id: params[:organization_id]
+    )
+    service.execute
 
-    if user && user.valid_password?(params[:user][:password])
-      if user.memberships.exists?(organization_id: params[:organization_id])
-        sign_in(:user, user)
-        session[:organization_id] = params[:organization_id]
-        redirect_to after_sign_in_path_for(user), notice: 'Successfully signed in!'
-      else
-        flash[:alert] = "You do not belong to the selected organization."
-        redirect_to new_session_path(:user)
-      end
+    if service.success?
+      sign_in(:user, service.user)
+      session[:organization_id] = params[:organization_id]
+      redirect_to after_sign_in_path_for(service.user), notice: 'Successfully signed in!'
     else
-      flash[:alert] = "Invalid email, password, or organization."
+      flash[:alert] = service.errors
       redirect_to new_session_path(:user)
     end
   end
